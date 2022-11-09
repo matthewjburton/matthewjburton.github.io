fff5cc86a90efb3616813a4be590ffc4541fc4ae
Matt Burton <mattjburton64@gmail.com>
2022-11-09 17:22:12 -0500

    completed connect dropdown

---
+++ javascript.js
@@ -0,0 +1,7 @@
+function copyEmail() { 
+     // Copy the text inside the text field
+    navigator.clipboard.writeText("mattjburton64@gmail.com");
+  
+    // Alert the copied text
+    alert("Copied the text: " + "mattjburton64@gmail.com");
+}
