b82ab70b2768667e4a7a2311e34c263fdc638f3f
Matt Burton <mattjburton64@gmail.com>
2022-11-15 19:39:52 -0500

    fonts and removed music

--- javascript.js
+++
@@ -1,7 +0,0 @@
-function copyEmail() { 
-     // Copy the text inside the text field
-    navigator.clipboard.writeText("mattjburton64@gmail.com");
-  
-    // Alert the copied text
-    alert("Copied the text: " + "mattjburton64@gmail.com");
-}
